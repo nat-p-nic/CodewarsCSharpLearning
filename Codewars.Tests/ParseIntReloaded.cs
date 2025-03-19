89904709d54be2c826d5ad91025accc4d9ee6983
Nathan Nicholson <nathan.nicholson@pendennis.com>
2025-03-19 13:07:25 +0000

    Completed 'parseInt() reloaded'
    https://www.codewars.com/kata/525c7c5ab6aecef16e0001a5

---
+++ Codewars.Tests/ParseIntReloaded.cs
@@ -0,0 +1,23 @@
+﻿namespace Codewars.Tests
+{
+    public class SolutionTest
+    {
+        [Test]
+        public void FixedTests1()
+        {
+            Assert.That(Parser.ParseInt("one"), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FixedTests2()
+        {
+            Assert.That(Parser.ParseInt("twenty"), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void FixedTests3()
+        {
+            Assert.That(Parser.ParseInt("two hundred forty-six"), Is.EqualTo(246));
+        }
+    }
+}
